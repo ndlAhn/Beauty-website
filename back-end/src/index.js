897a304a3fa157fbdc2a71ec3d4dec5d557643dc
Nguyễn Trung <ndlanh22052002@gmail.com>
2025-03-11 22:08:24 +0700

    11/03/2025: Cors

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }),
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
